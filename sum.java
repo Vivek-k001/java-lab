f09d61a93c656120abfd53f6d114d07e9adc097d
Vivek-k001 <vk3699010@gmail.com>
2026-01-16 15:55:28 +0530

    done

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class sum{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter num1 : ");
+		int a = sc.nextInt();
+
+		System.out.print("Enter num2 : ");
+		int b = sc.nextInt();
+
+		System.out.println("Sum = " + (a + b));
+
+	}
+}
+
