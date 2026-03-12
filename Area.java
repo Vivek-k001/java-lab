986572d7d85610f0a61b05747dba5adebc649741
Vivek2cold <vk3699010@gmail.com>
2026-03-12 13:20:07 +0530

    done

--- Area.java
+++ Area.java
@@ -1,23 +1,43 @@
+import java.util.Scanner;
+
 class Area {
 
     double area(double r){
-        return 3.14*r*r;
+        return 3.14 * r * r;
     }
 
-    int area(int l,int b){
-        return l*b;
+    int area(int l, int b){
+        return l * b;
     }
 
-    double area(double b,double h){
-        return 0.5*b*h;
+    double area(double b, double h){
+        return 0.5 * b * h;
     }
 
     public static void main(String args[]) {
 
+        Scanner sc = new Scanner(System.in);
         Area a = new Area();
 
-        System.out.println("Circle area:"+a.area(5));
-        System.out.println("Rectangle area:"+a.area(4,6));
-        System.out.println("Triangle area:"+a.area(3.0,4.0));
+        System.out.print("Enter radius of circle: ");
+        double r = sc.nextDouble();
+
+        System.out.print("Enter length of rectangle: ");
+        int l = sc.nextInt();
+
+        System.out.print("Enter breadth of rectangle: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter base of triangle: ");
+        double tb = sc.nextDouble();
+
+        System.out.print("Enter height of triangle: ");
+        double h = sc.nextDouble();
+
+        System.out.println("Circle area: " + a.area(r));
+        System.out.println("Rectangle area: " + a.area(l, b));
+        System.out.println("Triangle area: " + a.area(tb, h));
+
+        sc.close();
     }
 }
