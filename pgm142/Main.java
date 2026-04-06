1650b460b2ad566d825eca8232a1bceae1e34e6f
Vivek-k001 <vk3699010@gmail.com>
2026-04-06 15:38:33 +0530

    done

---
+++ pgm142/Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+// Base class
+class Student {
+    int rollNo;
+    float marks;
+
+    void getStudentData(Scanner sc) {
+        System.out.print("Enter Roll Number: ");
+        rollNo = sc.nextInt();
+
+        System.out.print("Enter Academic Marks: ");
+        marks = sc.nextFloat();
+    }
+
+    void displayStudentData() {
+        System.out.println("Roll Number: " + rollNo);
+        System.out.println("Academic Marks: " + marks);
+    }
+}
+
+// Interface (used instead of second base class)
+interface Sports {
+    float sportsScore = 0;
+
+    void getSportsData(Scanner sc);
+    void displaySportsData();
+}
+
+// Derived class
+class Result extends Student implements Sports {
+    float sportsScore;
+    float total;
+
+    public void getSportsData(Scanner sc) {
+        System.out.print("Enter Sports Score: ");
+        sportsScore = sc.nextFloat();
+    }
+
+    public void displaySportsData() {
+        System.out.println("Sports Score: " + sportsScore);
+    }
+
+    void calculate() {
+        total = marks + sportsScore;
+    }
+
+    void displayResult() {
+        displayStudentData();
+        displaySportsData();
+        System.out.println("Total Score: " + total);
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Result r = new Result();
+
+        r.getStudentData(sc);
+        r.getSportsData(sc);
+        r.calculate();
+
+        System.out.println("\n--- Student Result ---");
+        r.displayResult();
+
+        sc.close();
+    }
+}
